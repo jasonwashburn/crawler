318b48bfd1ced019ccbbef071da7e6db378ef933
Jason Washburn <jason.washburn@gmail.com>
2025-07-28 06:35:17 -0500

    feat: WIP - start work on getURLsFromHTML

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jasonwashburn/crawler
 
 go 1.23.6
+
+require golang.org/x/net v0.42.0 // indirect
--- html.go
+++ html.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-	return nil, nil
+	var links []string
+	htmlReader := strings.NewReader(htmlBody)
+	doc, err := html.Parse(htmlReader)
+	if err != nil {
+		return nil, err
+	}
+	for n := range doc.Descendants() {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					link := attr.Val
+					if !strings.HasPrefix(link, "http") {
+						// TODO: Handle relative URLs with ..
+						// TODO: Handle relative URLs starting with root
+						link = fmt.Sprintf("%s/%s", rawBaseURL, strings.TrimLeft(link, "/"))
+					}
+					links = append(links, link)
+				}
+			}
+		}
+	}
+	return links, nil
 }
