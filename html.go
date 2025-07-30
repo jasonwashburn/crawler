923c6582f970f25baf45ba708ac0f0b32455053d
Jason Washburn <jason.washburn@gmail.com>
2025-07-30 06:18:30 -0500

    refactor: improve url handling

--- html.go
+++ html.go
@@ -1,39 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"net/url"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-	var links []string
-	htmlReader := strings.NewReader(htmlBody)
-	doc, err := html.Parse(htmlReader)
+	baseURL, err := url.Parse(rawBaseURL)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to parse base URL: %w", err)
 	}
-	for n := range doc.Descendants() {
+
+	var links []string
+	doc, err := html.Parse(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse HTML: %w", err)
+	}
+
+	var visitNode func(*html.Node)
+	visitNode = func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, attr := range n.Attr {
 				if attr.Key == "href" {
-					link := attr.Val
-					if strings.HasPrefix(link, "http://") || strings.HasPrefix(link, "https://") {
-						links = append(links, link)
-					} else if strings.HasPrefix(link, "/") {
-						baseURLParts := strings.Split(rawBaseURL, "/")
-						baseURL := strings.Join(baseURLParts[:3], "/")
-						links = append(links, baseURL+link)
-					} else if strings.HasPrefix(link, "../") {
-						baseURLParts := strings.Split(rawBaseURL, "/")
-						baseURL := strings.Join(baseURLParts[:3], "/")
-						link = strings.TrimPrefix(link, "../")
-						link = baseURL + "/" + link
-						links = append(links, link)
+					link, err := url.Parse(attr.Val)
+					if err != nil {
+						continue
 					}
+					resolvedURL := baseURL.ResolveReference(link)
+					links = append(links, resolvedURL.String())
 				}
 			}
 		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			visitNode(c)
+		}
 	}
+
+	visitNode(doc)
 	return links, nil
 }
