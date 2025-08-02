e9e362b0ae7d088d0b41574466d6c7b906022868
Jason Washburn <jason.washburn@gmail.com>
2025-08-02 08:26:47 -0500

    feat: add recursive page crawling

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/jasonwashburn/crawler
 
 go 1.23.6
 
-require golang.org/x/net v0.42.0 // indirect
+require golang.org/x/net v0.42.0
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 )
 
@@ -20,9 +21,60 @@ func main() {
 
 	baseURL := args[1]
 	fmt.Println("starting crawl of: ", baseURL)
-	body, err := getHTML(baseURL)
-	if err != nil {
-		fmt.Println("error fetching HTML:", err)
+
+	pages := make(map[string]int)
+	crawlPage(baseURL, baseURL, pages)
+
+	for page, count := range pages {
+		fmt.Printf("Page: %s, Count: %d\n", page, count)
 	}
-	fmt.Println(body)
+}
+
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		fmt.Println("error parsing base URL:", err)
+		return
+	}
+	currentURL, err := url.Parse(rawCurrentURL)
+	if err != nil {
+		fmt.Println("error parsing current URL:", err)
+		return
+	}
+
+	if currentURL.Host != baseURL.Host {
+		fmt.Println("skipping external link:", currentURL.String())
+		return
+	}
+
+	normalizedURL, err := normalizeURL(currentURL.String())
+	if err != nil {
+		fmt.Println("error normalizing URL:", err)
+		return
+	}
+
+	if _, exists := pages[normalizedURL]; exists {
+		pages[normalizedURL]++
+		fmt.Println("already crawled:", normalizedURL, "count:", pages[normalizedURL])
+	} else {
+		pages[normalizedURL] = 1
+		fmt.Println("crawling:", currentURL.String())
+		html, err := getHTML(currentURL.String())
+		if err != nil {
+			fmt.Println("error fetching HTML for", currentURL.String(), ":", err)
+			return
+		}
+
+		urls, err := getURLsFromHTML(html, rawBaseURL)
+		if err != nil {
+			fmt.Println("error extracting links from", normalizedURL, ":", err)
+			return
+		}
+
+		fmt.Printf("Extracted %d links from %s\n", len(urls), normalizedURL)
+
+		for _, link := range urls {
+			crawlPage(rawBaseURL, link, pages)
+		}
+	}
 }
